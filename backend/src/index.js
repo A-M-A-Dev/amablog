a6785f77c349907862e99152cdc4967f1339cd15
ahmadsalimi <ahsa9978@gmail.com>
2021-01-03 16:10:01 +0330

    add methodNotAllowed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,8 @@ import express from "express"
 import bodyParser from "body-parser"
 import cookieParser from "cookie-parser"
 import mongoose from "mongoose"
-import { fullUrl } from "./utils.js"
 import userRouter from "./routers/user.router.js"
+import { notFound } from "./controllers/default.controller.js"
 
 const hostname = "0.0.0.0";
 const port = 80;
@@ -20,24 +20,11 @@ mongoose
 
 const app = express()
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 app.use('/api', userRouter)
-
-app.all("*", (req, res) => {
-    if (req.accepts('json')) {
-        res.status(404);
-        res.json({
-            message: `${req.method}: ${fullUrl(req)} not found!`
-        });
-    } else {
-        res.status(404);
-        res.type('txt').send('Not found');
-    }
-});
+app.all("*", notFound);
 
 app.listen(port, hostname, () => {
     console.log(`server running at http://${hostname}:${port}/`);
--- backend/src/routers/user.router.js
+++ backend/src/routers/user.router.js
@@ -1,11 +1,20 @@
 import express from "express"
 import { signup, signin, testAuth } from "../controllers/user.controller.js"
 import { authorize } from "../services/auth.service.js"
+import { generateMethodNOtAllowed } from "../controllers/default.controller.js"
 
 const router = express.Router()
 
-router.post('/signup', signup)
-router.post('/signin', signin)
-router.get('/test_auth', authorize, testAuth)
+router.route('/signup')
+    .post(signup)
+    .all(generateMethodNOtAllowed('POST'))
+
+router.route('/signin')
+    .post(signin)
+    .all(generateMethodNOtAllowed('POST'))
+
+router.route('/test_auth')
+    .get(authorize, testAuth)
+    .all(generateMethodNOtAllowed('GET'))
 
 export default router
